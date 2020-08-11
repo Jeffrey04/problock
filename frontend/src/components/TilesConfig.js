609edc0ec1ef4d42d5961c4eaac4fcf73e08fce0
Jeffrey04 <mycyberpet@yahoo.com>
2020-08-11 13:55:44 +0800

    rule condition should now editable

--- frontend/src/components/TilesConfig.js
+++ frontend/src/components/TilesConfig.js
@@ -2,6 +2,7 @@ import {
   DISPLAY_TYPE,
   TRIGGER_TYPE,
   tilesUpdateDisplay,
+  tilesUpdateEvent,
   tilesUpdatePropertyNeighbours,
 } from "../features/tiles/TilesSlice";
 import { GAME_CONFIG, UIUpdateConfig } from "../features/ui/UISlice";
@@ -19,6 +20,29 @@ import React from "react";
 import { TextField } from "@material-ui/core";
 import TilePropertyNeighbours from "./TilePropertyNeighbours";
 import Typography from "@material-ui/core/Typography";
+import ruler from "ruler";
+import { updateError } from "../features/error/ErrorSlice";
+
+function conditionOnChange(event, events, index_event, index_rule) {
+  return events.map((incoming, idx_event) => {
+    if (idx_event === index_event) {
+      return {
+        trigger: incoming.trigger,
+        actions: incoming.actions.map((rule, idx_rule) =>
+          idx_rule === index_rule
+            ? { condition: event.target.value, set: rule.set }
+            : rule
+        ),
+      };
+    } else {
+      return incoming;
+    }
+  });
+}
+
+function ruleIsValid(event) {
+  return ruler.parse(event.target.value) instanceof Function;
+}
 
 export default function () {
   const isOpened = useSelector(
@@ -27,6 +51,7 @@ export default function () {
   const dispatch = useDispatch();
   const properties = useSelector((state) => state.tiles.properties);
   const events = useSelector((state) => state.tiles.events);
+  const error = useSelector((state) => state.error);
 
   return (
     <Modal
@@ -95,7 +120,12 @@ export default function () {
             </div>
             <Typography variant="h3">Events</Typography>
             {events.map((event_tile, index_tile) => (
-              <FormControl fullWidth margin="normal" component="fieldset">
+              <FormControl
+                key={index_tile}
+                fullWidth
+                margin="normal"
+                component="fieldset"
+              >
                 <FormLabel component="legend">Event {index_tile + 1}</FormLabel>
                 <FormControl fullWidth margin="normal" component="fieldset">
                   <FormLabel component="legend">Trigger</FormLabel>
@@ -120,18 +150,39 @@ export default function () {
                     ></TextField>
                   )}
                 </FormControl>
-                {event_tile.actions.map((action, index_action) => (
-                  <FormControl fullWidth margin="normal" component="fieldset">
-                    <FormLabel component="legend">
-                      Action {index_action}
-                    </FormLabel>
+                {event_tile.actions.map((rule, index_rule) => (
+                  <FormControl
+                    key={index_rule}
+                    fullWidth
+                    margin="normal"
+                    component="fieldset"
+                  >
+                    <FormLabel component="legend">Rule {index_rule}</FormLabel>
                     <TextField
                       fullWidth
-                      mukltiline
-                      id={"event-trigger-value-".concat(index_tile)}
+                      id={"event-condition-value-".concat(index_tile)}
                       label="Condition"
-                      value={JSON.stringify(action.condition)}
+                      value={rule.condition}
                       size="medium"
+                      error={
+                        error.id === "event-condition-value-".concat(index_tile)
+                      }
+                      onChange={(e) => {
+                        dispatch(
+                          tilesUpdateEvent(
+                            conditionOnChange(e, events, index_tile, index_rule)
+                          )
+                        );
+
+                        if (ruleIsValid(e) === false) {
+                          dispatch(
+                            updateError({
+                              id: "event-condition-value-".concat(index_tile),
+                              message: "Condition is malformed",
+                            })
+                          );
+                        }
+                      }}
                     ></TextField>
                     <FormControl fullWidth margin="normal" component="fieldset">
                       <FormLabel component="legend">Set value</FormLabel>
@@ -139,7 +190,7 @@ export default function () {
                         fullWidth
                         id={"event-trigger-value-".concat(index_tile)}
                         label="field"
-                        value={action.set.field}
+                        value={rule.set.field}
                         size="medium"
                       ></TextField>
                       <TextField
@@ -147,7 +198,7 @@ export default function () {
                         multiline
                         id={"event-trigger-value-".concat(index_tile)}
                         label="rule"
-                        value={JSON.stringify(action.set.rule)}
+                        value={rule.set.rule}
                         size="medium"
                       ></TextField>
                     </FormControl>
